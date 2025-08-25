802c33872ebc86d755d0bf3d4fb8f22f6f065123
tuanle13-git <tuanlv1303@gmail.com>
2025-08-25 23:43:45 +0700

    kiểu cơ bản

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func typeGo() {
+	var x bool          // boolean mặc đinh là false
+	var Float32 float32 // int mặc định là 0
+	var Float64 float64 // int mặc định là 0
+	fmt.Println(Float32, Float64, x)
+}
 func main() {
-    fmt.Println("Hello, World!")
+	typeGo()
+	fmt.Println("Hello, World!")
 }
